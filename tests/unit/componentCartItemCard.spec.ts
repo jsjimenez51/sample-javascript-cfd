ca19ff64d9419b9aea8c93e2c00e28c8dbd4d9f4
Josh Jimenez <josh.jimenez@circleci.com>
2021-11-08 10:47:11 -0800

    test dog

--- tests/unit/componentCartItemCard.spec.ts
+++ tests/unit/componentCartItemCard.spec.ts
@@ -5,7 +5,7 @@ describe('CartItemCard.vue', () => {
     it('renders the menuItem prop data', () => {
         const menuItem = {
             id: 1,
-            name: "French Pizza",
+            name: "French Dog",
             description: "Fried potato rectangles",
             price: "4.99",
             imageId: '1'
